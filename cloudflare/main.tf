a37f36693ef7c3f309cd1859ff525f4edeb57368
Gyula Asztalos <137063656+gyulaasztalos@users.noreply.github.com>
2025-07-03 16:18:45 +0200

    trigger CI

--- cloudflare/main.tf
+++ cloudflare/main.tf
@@ -1,6 +1,7 @@
 # Configure the Cloudflare provider using the required_providers stanza
 # required with Terraform 0.13 and beyond. You may optionally use version
 # directive to prevent breaking changes occurring unannounced.
+
 terraform {
   cloud {
     organization = "asztalosgyula"
