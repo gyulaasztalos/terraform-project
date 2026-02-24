71a47ae1aca959b2e700422ac5347d5087b0e715
Gyula Asztalos <137063656+gyulaasztalos@users.noreply.github.com>
2026-02-24 12:38:30 +0100

    fix

--- backblaze/outputs.tf
+++ backblaze/outputs.tf
@@ -30,13 +30,34 @@ output "nas_bucket_id" {
 }
 
 output "nas_application_key_id" {
-  description = "Application Key ID for CNPG backups"
+  description = "Application Key ID for NAS backups"
   value       = b2_application_key.nas_backup_key.application_key_id
 }
 
 output "nas_application_key" {
-  description = "Application Key for CNPG backups"
+  description = "Application Key for NAS backups"
   value       = b2_application_key.nas_backup_key.application_key
+  sensitive   = true
+}
+
+output "nas_bucket_name" {
+  description = "Name of the created B2 bucket"
+  value       = b2_bucket.nas_backup.bucket_name
+}
+
+output "nas_bucket_id" {
+  description = "ID of the created B2 bucket"
+  value       = b2_bucket.nas_backup.bucket_id
+}
+
+output "etcd_application_key_id" {
+  description = "Application Key ID for etcd backups"
+  value       = b2_application_key.etcd_backup_key.application_key_id
+}
+
+output "etcd_application_key" {
+  description = "Application Key for etcd backups"
+  value       = b2_application_key.etcd_backup_key.application_key
   sensitive   = true
 }
 
